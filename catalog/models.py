2a49ebe1c209b3f3064f80b939613adfd2d66b4f
mihail <mihailerohin235@gmail.com>
2023-08-28 22:15:02 +0300

    add new model Blog

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
     product_name = models.CharField(max_length=25, verbose_name='наименование')
     description = models.CharField(max_length=255, verbose_name='описание')
-    preview = models.ImageField(upload_to='products/', verbose_name='превью', null=True)
+    preview = models.ImageField(upload_to='products/', verbose_name='превью', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена')
     production_data = models.DateField(verbose_name='дата создания', auto_now_add=True)
@@ -28,3 +30,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='slug')
+    contents = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='media/', verbose_name='Изображение', **NULLABLE)
+    date_production = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    is_publication = models.BooleanField(verbose_name='Опубликовано', default=True)
+    views_count = models.IntegerField(verbose_name='Просмотры', default=0)
+
+    def __str__(self):
+        return self.blog_title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
